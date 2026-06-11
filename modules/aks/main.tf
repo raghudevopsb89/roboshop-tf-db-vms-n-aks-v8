611aca01c48d8a11c9dd3931b437885a9b65b658
Raghu K <raghuk.vit@gmail.com>
2026-06-11 07:02:38 +0530

    terraform: update 1 file(s) (main.tf)

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -44,5 +44,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "pool1" {
   vm_size               = "Standard_D2s_v3"
   node_count            = 2
   vnet_subnet_id        = var.subnet_id
+
+  lifecycle {
+    ignore_changes = [upgrade_settings]
+  }
+
 }
 
