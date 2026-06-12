4fa525f62026090564ef9e3185a889ad37870bcf
Raghu K <raghuk.vit@gmail.com>
2026-06-12 06:46:18 +0530

    terraform: update 1 file(s) (helm.tf)

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -48,7 +48,31 @@ resource "helm_release" "prometheus_stack" {
   ]
 }
 
+## External DNS Helm chart secret
+resource "null_resource" "external-dns-secret" {
+  depends_on = [
+    null_resource.kube-config
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+echo '{
+  "tenantId": "229f3fa3-57f3-4e2c-852f-24b7bf512640",
+  "subscriptionId": "3f2e42e1-ca06-4a99-8c56-be8d8ba306db",
+  "resourceGroup": "${var.rg_name}",
+  "aadClientId": "${data.azurerm_key_vault_secret.ClientID.value}",
+  "aadClientSecret": "${data.azurerm_key_vault_secret.ClientPassword.value}"
+}' >/tmp/azure.json
+kubectl create secret generic azure-config-file --namespace devops --from-file /tmp/azure.json
+EOF
+  }
+
+}
+
 resource "helm_release" "external_dns" {
+
+  depends_on = [null_resource.external-dns-secret]
+
   chart      = "external-dns"
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns"
