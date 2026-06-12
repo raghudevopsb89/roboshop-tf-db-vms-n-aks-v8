79fe88addae219ef79b382ec1650d04f6ff01c12
Raghu K <raghuk.vit@gmail.com>
2026-06-12 06:41:03 +0530

    terraform: add 1 file(s) (data.tf)

---
+++ modules/aks/data.tf
@@ -0,0 +1,10 @@
+data "azurerm_key_vault_secret" "ClientID" {
+  name         = "externalDnsClientID"
+  key_vault_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.KeyVault/vaults/roboshopb89"
+}
+
+data "azurerm_key_vault_secret" "ClientPassword" {
+  name         = "ExternalDnsClientPassword"
+  key_vault_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.KeyVault/vaults/roboshopb89"
+}
+
